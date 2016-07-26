47b32b92e9eca8232f5474997447648617d4addb
Rincewind76 <thomas@schoedl.me>
2016-07-26 18:36:56 +0200

    Add files via upload

--- 76_SMAInverter.pm
+++ 76_SMAInverter.pm
@@ -33,6 +33,8 @@ use IO::Socket::INET;
 use DateTime;
 
 # Global vars
+my $globname = "SMAInverter";
+
 # Sleep Mode variables
 my $default_starthour = "05:00";
 my $starthour = 5;
@@ -46,8 +48,24 @@ my $suppress_night_mode = 0;
 my $detail_level = 0;
 # General enabling of the module
 my $modulstate_enabled = 0;
-# Alarm levels absed on power
+# Alarm levels based on power
 my ($alarm_value1,$alarm_value2,$alarm_value3);
+# protocol related
+my $mysusyid = 233;								# random number, has to be different from any device in local network
+my $myserialnumber = 123321123;		# random number, has to be different from any device in local network
+my $target_susyid = 0xFFFF;				# 0xFFFF is any susyid
+my $target_serial = 0xFFFFFFFF;		# 0xFFFFFFFF is any serialnumber
+my $default_target_susyid = 0xFFFF;				# 0xFFFF is any susyid
+my $default_target_serial = 0xFFFFFFFF;		# 0xFFFFFFFF is any serialnumber
+my $pkt_ID = 0x8001;						# Packet ID
+
+# Inverter Data fields
+my $inv_susyid = 0;
+my $inv_serial = 0;
+
+#Return values
+my $r_OK = 0;		# Everything OK
+my $r_FAIL = 1;	# Operation failed
 
 
 ###################################
@@ -302,9 +320,213 @@ sub SMAInverter_Attr(@)
 #####################################
 sub SMAInverter_GetStatus($)
 {
-		# Nothing here yet...
+	my ($hash) = @_;
+	my $name = $hash->{NAME};
+	my $interval = $hash->{INTERVAL};
+	
+	# For logging events set the module name
+	$globname = $name;
+	
+	if(SMA_logon($hash->{Host}, $hash->{Pass}) eq $r_OK)
+	{
+			Log3 $globname, 5, "$globname: Logged in now";
+			
+			# nothing more to do, just log out
+			SMA_logout($hash->{Host});
+	} else
+	{
+	}
+	
+	InternalTimer(gettimeofday()+$interval, "SMAInverter_GetStatus", $hash, 1);
 }
 
+####################################
+sub SMA_logon($$)
+{
+	my $host = $_[0];
+	my $pass = $_[1];
+	my $cmdheader = "534D4100000402A00000000100";
+	my $pktlength = "3A";		# length = 58 for logon command
+	my $esignature = "001060650EA0";
+	my $cmd = "";
+	my $timestmp = "";
+	my $myID = "";
+	my $target_ID = "";
+	my $spkt_ID = "";
+	my $cmd_ID = "";
+	my ($socket,$data,$size);
+	
+	use constant MAXBYTES => scalar 100;
+	
+	#Encode the password
+	my $encpasswd = "888888888888888888888888"; # template for password	
+	for my $index (0..length $pass )	# encode password
+	{
+		substr($encpasswd,($index*2),2) = substr(sprintf ("%lX", (hex(substr($encpasswd,($index*2),2)) + ord(substr($pass,$index,1)))),0,2);
+	}
+	
+	# Get current timestamp in epoch format (unix format)
+	$timestmp = ByteOrderLong(sprintf("%08X",int(time())));
+	
+	# Define own ID and target ID and packet ID
+	$myID = ByteOrderShort(substr(sprintf("%04X",$mysusyid),0,4)) . ByteOrderLong(sprintf("%08X",$myserialnumber));
+	$target_ID = ByteOrderShort(substr(sprintf("%04X",$target_susyid),0,4)) . ByteOrderLong(sprintf("%08X",$target_serial));
+	$pkt_ID = 0x8001;	# Reset to 0x8001
+	$spkt_ID = ByteOrderShort(sprintf("%04X",$pkt_ID));
+	
+	#Logon command
+	$cmd_ID = "0C04FDFF" . "07000000" . "84030000";  # Logon command + User group "User" + (maybe) Timeout
+	
+	#build final command to send
+	$cmd = $cmdheader . $pktlength . $esignature . $target_ID . "0001" . $myID . "0001" . "00000000" . $spkt_ID . $cmd_ID . $timestmp . "00000000" . $encpasswd . "00000000";
+
+	# flush after every write
+	$| = 1; 				
+	
+	# Create Socket and check if successful
+	$socket = new IO::Socket::INET (PeerHost => $host, PeerPort => 9522, Proto => 'udp',); # open Socket
+
+	if (!$socket) { 															
+		# in case of error
+		Log3 $globname, 1, "$globname: ERROR. Can't open socket to inverter: $!";
+		return $r_FAIL;
+	};
+
+	# Send Data
+	$data = pack("H*",$cmd);
+	$socket->send($data);
+	Log3 $globname, 4, "$globname: Send login to $host on Port 9522 with password $pass ";
+	Log3 $globname, 5, "$globname: Send: $cmd ";
+	
+	# Receive Data and do a first check regarding length
+	eval 
+	{
+		local $SIG{ALRM} = sub { die "alarm time out" };
+		alarm 5;
+		# receive data
+		$socket->recv($data, MAXBYTES) or die "recv: $!";					
+		$size = length($data);
+
+		# check if something was received
+		if (defined $size)															
+		{
+			my $received = unpack("H*", $data);
+			Log3 $globname, 5, "$globname: Received: $received";
+			
+		}
+		
+		alarm 0;
+		1;																	
+	} or Log3 $globname, 1, "$globname query timed out";
+	
+	# Nothing received -> exit loop
+	if (not defined $size)															
+	{
+		Log3 $globname, 1, "$globname: Nothing received...";
+		# send: cmd_logout
+		SMA_logout($host);
+		$socket->close();
+		return $r_FAIL;
+	} else
+	{
+		# We have received something!
+		
+		if ($size > 62)
+		{
+			# Check all parameters of answer
+			my $r_susyid = unpack("V*", substr $data, 20, 2);
+			my $r_serial = unpack("V*", substr $data, 22, 4);
+			my $r_pkt_ID = unpack("V*", substr $data, 40, 2);
+			my $r_cmd_ID = unpack("V*", substr $data, 42, 4);
+			my $r_error  = unpack("V*", substr $data, 36, 4);
+			if (($r_susyid ne $mysusyid) || ($r_serial ne $myserialnumber) || ($r_pkt_ID ne $pkt_ID) || ($r_cmd_ID ne 0xFFFD040D) || ($r_error ne 0))
+			{
+				# Response does not match the parameters we have sent, maybe different target
+				Log3 $globname, 1, "$globname: Inverter answer does not match our parameters.";
+				Log3 $globname, 5, "$globname: Request/Response: SusyID $mysusyid/$r_susyid, Serial $myserialnumber/$r_serial, Packet ID $pkt_ID/$r_pkt_ID, Command 0xFFFD040D/$r_cmd_ID, Error $r_error";
+				# send: cmd_logout
+				SMA_logout($host);
+				$socket->close();
+				return $r_FAIL;
+			}
+			# ******************************************************************
+			
+		} else
+		{
+			Log3 $globname, 1, "$globname: Format of inverter response does not fit.";
+			# send: cmd_logout
+			SMA_logout($host);
+			$socket->close();
+			return $r_FAIL;
+		}
+	}
+	
+	# All seems ok, logged in! 
+	$socket->close();	
+	return $r_OK;
+}
+
+####################################
+sub SMA_logout($)
+{
+	my $host = $_[0];
+	my $cmdheader = "534D4100000402A00000000100";
+	my $pktlength = "22";		# length = 34 for logout command
+	my $esignature = "0010606508A0";
+	my $cmd = "";
+	my $timestmp = "";
+	my $myID = "";
+	my $target_ID = "";
+	my $spkt_ID = "";
+	my $cmd_ID = "";
+	my ($socket,$data,$size);
+	
+	# Define own ID and target ID and packet ID
+	$myID = ByteOrderShort(substr(sprintf("%04X",$mysusyid),0,4)) . ByteOrderLong(sprintf("%08X",$myserialnumber));
+	$target_ID = ByteOrderShort(substr(sprintf("%04X",$target_susyid),0,4)) . ByteOrderLong(sprintf("%08X",$target_serial));
+	# Increasing Packet ID
+	$pkt_ID = $pkt_ID + 1;	
+	$spkt_ID = ByteOrderShort(sprintf("%04X",$pkt_ID));
+	
+	#Logout command
+	$cmd_ID = "0E01FDFF" . "FFFFFFFF";  # Logout command
+
+	#build final command to send
+	$cmd = $cmdheader . $pktlength . $esignature . $target_ID . "0003" . $myID . "0003" . "00000000" . $spkt_ID . $cmd_ID . "00000000";
+
+	# flush after every write
+	$| = 1; 				
+	
+	# Create Socket and check if successful
+	$socket = new IO::Socket::INET (PeerHost => $host, PeerPort => 9522, Proto => 'udp',); # open Socket
+
+	if (!$socket) { 															
+		# in case of error
+		Log3 $globname, 1, "$globname: ERROR. Can't open socket to inverter: $!";
+		return $r_FAIL;
+	};
+	
+	$socket->close();	
+	return $r_OK;	
+}
+
+####################################
+sub ByteOrderShort($)
+{
+	my $input = $_[0];
+	my $output = "";
+	$output = substr($input, 2, 2) . substr($input, 0, 2);
+	return $output;
+}
+
+####################################
+sub ByteOrderLong($)
+{
+	my $input = $_[0];
+	my $output = "";
+	$output = substr($input, 6, 2) . substr($input, 4, 2) . substr($input, 2, 2) . substr($input, 0, 2);
+	return $output;
+}
 1;
 
 =pod
